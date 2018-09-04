d24a85a4c866858edeb3d85012d34012860f3c20
andes <s.gilparga@gmail.com>
2018-09-04 13:28:55 -0500

    Commit incial con la arquitectura básica del proyecto

---
+++ sobremesa-app/server/server.js
@@ -0,0 +1,32 @@
+﻿var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+ 
+var app = express();
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+ 
+var server = app.listen(3000, function() {
+    var host = 'localhost';
+    var port = server.address().port;
+    console.log('App listening at http://%s:%s', host, port);
+});
+ 
+module.exports = app;
