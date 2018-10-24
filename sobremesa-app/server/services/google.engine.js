d101b5aff8cc74633628bd128f5f3f418bdb5652
DESKTOP-NQM9AOU\s.gil1002 <s.gilparga@gmail.com>
2018-10-24 07:36:00 -0500

    Inicio integración del engine de google

--- sobremesa-app/server/services/google.engine.js
+++ sobremesa-app/server/services/google.engine.js
@@ -1,6 +1,7 @@
 var axios = require('axios');
 var textService = require('../services/text.service');
-var url = "https://www.vam.ac.uk/api/json/museumobject/search?q=";
+
+var url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDERGecwMwWjc-C-YdC02bzi0Pw6QKFFHE&cx=002610522876290491785:-vtu8psekmi&searchType=image&rights=cc_noncommercial&q=";
 
 var engine = {};
 
@@ -9,7 +10,7 @@ function createSerachURL(meta){
         if(meta.charAt(i) === ' ')
             meta = textService.setCharAt(meta,i,'+');
     }
-    console.log("V&A searching for: " + meta);
+    console.log("Google searching for: " + meta);
     return meta;    
 }
 
@@ -18,10 +19,9 @@ function createPhotoArray(photos){
     if(photos.length > 0)
     {
     for(i = 0; i < photos.length; i++){
-        var key = photos[i].fields.primary_image_id;
         var photo = {
-            source : "V&A",
-            uri : "http://media.vam.ac.uk/media/thira/collection_images/" + key.substring(0,6) + "/" + key + ".jpg"
+            source : "google",
+            uri : photos[i].link
         }
         photoArray.push(photo);
     }
@@ -32,10 +32,10 @@ function createPhotoArray(photos){
 engine.search = function(meta){
     return new Promise(function(resolve, reject){ 
         axios.get(url + createSerachURL(meta)).then(function(results){
-             var photos = results.data.records;
+             var photos = results.items;
             resolve(createPhotoArray(photos),"");
         }, function(error){
-            resolve([], "Problemas al conectarse con V&A Museum: " + error);
+            resolve([], "Problemas al conectarse con Flickr: " + error);
         });
     });    
 }
